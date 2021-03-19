ccdd349c7721d27ad795e8fb4de9cf9c123ba0ca
wassimhmani <hmani.wassim.aiesec@gmail.com>
2021-03-19 11:04:11 +0100

    second commit

---
+++ src/components/init/init.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './init.css';
+
+function Init() {
+    return (<div class="glitch">
+    <div class="glitch__item"></div>
+    <div class="glitch__item"></div>
+    <div class="glitch__item"></div>
+    <div class="glitch__item"></div>
+    <div class="glitch__item"></div>
+    <h1 class="glitch__title">TuniBet</h1>
+  </div>)
+}
+export default Init;
--- src/components/live/live.js
+++ src/components/live/live.js
@@ -1,5 +1,43 @@
 import React from 'react'
-function Live(){
-    return (<h1>live works!!!</h1>)
+import './live.css'
+function Live() {
+    return (<div class="container">
+        <div class="box">
+            <div class="imgBox">
+                <img src="https://i.imgur.com/AzH17aZ.png" alt="a"/>
+              </div>
+                <div class="content">
+                    <p class="pepe">Football</p>
+                    <h2>Image Hover Effects</h2>
+                    <p class="pe">Lorem ipsum nisl vivamus viverra per facilisis morbi arcu tortor, varius blandit ligula fermentum sapien mi suspendisse ac orci, imperdiet hendrerit sem quam netus pellentesque luctus pretium.</p>
+                </div>
+            </div>
+            <div class="box">
+                <div class="imgBox">
+                    <img src="https://igx.4sqi.net/img/general/600x600/13391650_-Orrd3ODh6ekCbrvu0J8vv3NzETxcZyE3DypFTJ0r-g.jpg" alt="a"/>
+        </div>
+                    <div class="content">
+                        <p class="pepe" >Is</p>
+                        <h2>Image Hover Effects</h2>
+                        <p class="pe">Lorem ipsum nisl vivamus viverra per facilisis morbi arcu tortor, varius blandit ligula fermentum sapien mi suspendisse ac orci, imperdiet hendrerit sem quam netus pellentesque luctus pretium.</p>
+                    </div>
+                </div>
+                <div class="box">
+                    <div class="imgBox">
+                        <img src="https://www.sul21.com.br/wp-content/uploads/2018/06/20180615-france-600x600.jpg" alt="a" />
+
+        </div>
+                        <div class="content">
+                            <p class="pepe" >Everything</p>
+                            <h2>Image Hover Effects</h2>
+                            <p class="pe">Lorem ipsum nisl vivamus viverra per facilisis morbi arcu tortor, varius blandit ligula fermentum sapien mi suspendisse ac orci, imperdiet hendrerit sem quam netus pellentesque luctus pretium.</p>
+                        </div>
+                    </div>
+
+
+
+
+
+                </div>)
 }
 export default Live;
